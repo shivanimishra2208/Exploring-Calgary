a56857511ee8c84eb94b33a4aeb89598e7c9401e
VIVEK MISHRA <shivanimishra2208@gmail.com>
2022-08-15 16:56:00 -0600

    delete and patch APIs

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 
 deleteSVG='<button id="delete" class="btn btn-dark">Delete</button>'
 editSVG=  '<button id="edit" class="btn btn-light">Edit</button>'
- updateSVG = ' <button id="update" class= "btn btn-danger">update</button>'
+ //updateSVG = ' <button id="update" class= "btn btn-danger">update</button>'
 
 
 const userText=document.querySelector("#addcomment")
@@ -47,13 +47,13 @@ if(Array.isArray(savedComment)){
   }
 })
 
-const saveComment= async title =>{
+const saveComment= async titles =>{
   const options={
     method: 'POST',
     headers:{
       "Content-Type" : 'application/json'
     },
-    body: JSON.stringify(title)
+    body: JSON.stringify(titles)
   }
 
   try{
@@ -66,92 +66,90 @@ const saveComment= async title =>{
   }
 }
 
-const editCommentText= async title=>{
+const editCommentText= async titles=>{
   const options={
     method: 'PATCH',
     headers:{
       "Content-Type" : 'application/json'
     },
-    body: JSON.stringify(title)
+    body: JSON.stringify(titles)
   }
 
   try{
-    const response = await fetch(`${uri}/${title._id}`, options)
-    const data =  response.json()
+    const response = await fetch(`${uri}/${titles._id}`, options)
+    const data = await response.json()
+      arr=data
+      console.log(`${uri}/${titles._id}`)
 
-    console.log(`${uri}/${title._id}`)
   }
   catch(err){
     console.error(err.message)
   }
 }
 
-
-
-
-
  //fuction for deleting Id from array
- deleteComment=(id)=>{
-  const index= arr.findIndex(title=> title.id===id)
-  if(index!== -1)
-  arr.splice(index, 1)
-  saveComment(arr)
-
+ deleteComment=async id =>{
+  const index= arr.findIndex(titles=> titles._id===id)
+  if(index!== -1){
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+  }
 }
+ try {
+  const response = await fetch(`${uri}/${id}`, options)
+  await response.json()
+} catch (err) {
+  console.error(err.message)
+  //arr.splice(index, 1)
+  //saveComment(arr)
+}
+ }
 const editComment = (id, newtext) => {
-  const index = arr.findIndex(titles => titles._id === id)
-  if (index !== -1) arr[index].titles = newtext
+  const index = arr.findIndex(titles=> titles._id === id)
+  if (index !== -1){ 
+    arr[index].title = newtext.textContent
   editCommentText(arr[index])
   //saveComment(arr)
 }
+}
 
-const addlist = inputText=>{
+const addlist = (inputText,id)=>{
     if (!inputText){
         alert("Comment section is Empty")
          return;}
     
-
-
 const mainDiv=document.createElement("div")
     mainDiv.classList.add("list");
-    
 //updiv for commentbox repetition
- const updiv= document.createElement("li")
-  updiv.innerText=inputText
-  updiv.classList.add("newlist")
-  mainDiv.appendChild(updiv);
+const updivtext= document.createElement("li")
+updivtext.innerText=inputText
+updivtext.setAttribute("id",id)
+const textId= updivtext.getAttribute("id")
+updivtext.classList.add("newlist")
+mainDiv.appendChild(updivtext);
+
  //const textId =mainDiv.getAttribute("id", getId())
 
   //span for edit and delete button repetition
- const edit  = document.createElement("span");
+  const edit  = document.createElement("span");
   edit.innerHTML= editSVG
   edit.classList.add("editbutton")
   mainDiv.appendChild(edit)
 
-  edit.addEventListener("click", (event)=>{
-    //userText.value=inputText
-    updiv.focus()
-    updiv.contentEditable="true"
-    event.target.addEventListener("click",()=>{
-      const updated=document.createElement("update")
-      updated.innerHTML= updateSVG
-      edit.replaceWith(updated)
-      
-      mainDiv.appendChild(updated)
+  edit.addEventListener("click", ()=>{
+    updivtext.focus()
+    updivtext.contentEditable= "true"
+    editComment(textId,updivtext.innerText)
+    })// mainDiv.remove
 
-      const newtext= document.getElementsByClassName("newlist")
-      updated.addEventListener("click",()=>{
-       updiv.newtext
-       updated.replaceWith(edit);
-      
-    })
-   
+   updivtext.addEventListener("blur", ()=>{
+    updivtext.contentEditable="false"
+    editComment(textId,updivtext)
   })
-   // mainDiv.remove
-   updiv.addEventListener("blur", ()=>{
-    updiv.contentEditable="false"
-  })
-  })
+  
 const deleted =   document.createElement("span")
  deleted.innerHTML= deleteSVG
  deleted.classList.add("deletebutton")
@@ -159,7 +157,8 @@ const deleted =   document.createElement("span")
 
  deleted.addEventListener("click", ()=>{
        mainDiv.remove(),
-       deleteComment(id)
+       deleteComment(_id)
+         
      
  })
 
